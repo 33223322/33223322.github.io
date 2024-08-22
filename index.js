199fc95b77e7bd193a1bd4c7e699ff744e6f190d
May <98865137+MdeMay1@users.noreply.github.com>
2024-08-22 00:00:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var bienvenida = "Recuerdas el apodo que te puse¿ aver, cual es✨: ";
 var data = prompt(bienvenida);
 var comentario = "Asi es, Mi hermosa pepita";
 alert(comentario);
-var aea = "Sabes es que, no se como volver hablarte, despues de ser un completo idiota contigo, asi que te hice esto<3"
+var aea = "Sabes es que, no se como volver hablarte, despues de ser un completo idiota contigo"
 alert(aea);
-var a = "Se que no sera mucho, pero quiero que arreglemos lo nuestro, no te quiero perder porfavor"
+var a = "Se que esto no es mucho, pero espero te saque una sonrisita:)"
 alert(a);
+var e = "Come, toma awita, Abrigate y cuidate mucho miamor... es que me importa tu vida<3"
+alert(e);
 //Imprimir el nombre de la persona y transformalo a mayúsculas
 document.getElementById("data").innerHTML = data.toUpperCase();
 
@@ -13,8 +15,8 @@ const yesBtn = document.querySelector('#yesBtn');
 
 //ESCRIBIR LO QUE PASARA EN CASO DE QUE SÍ
 yesBtn.addEventListener('click', function(){
-    alert('Sabia que me ibas a perdonar:c, lo siento, teamo mucho no me dejes'); //Puedes mandar los mensajes que quieras
-    location.href = 'https://music.youtube.com/watch?v=coPv-QR3PZU'; //Cambiar el URL en YouTube de la canción que quieras 
+    alert('Sabia que me ibas a perdonar:c, lo siento, teamo mucho no me dejes, no quero que te vayas, no quero irme, soy muy feliz estando contigo, cuando estoy contigo, estoy en mi hogar, no quiero que se vaya eso:c PORFAVOR:cccc TE EXTRAÑO TANTO'); //Puedes mandar los mensajes que quieras
+    location.href = 'https://www.youtube.com/watch?v=VEsf5CfpLMI'; //Cambiar el URL en YouTube de la canción que quieras 
 });
 
 const noBtn = document.querySelector('#noBtn');
